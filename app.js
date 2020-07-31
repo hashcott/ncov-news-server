8d6402dcebdad2e04a7b84227d10362cd357a2b4
Nguyen Duc Hanh <1851061469@e.tlu.edu.vn>
2020-07-31 23:58:46 +0700

    change auto port

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // server.js
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('clean.json')
-const middlewares = jsonServer.defaults()
- 
-server.use(middlewares)
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("clean.json");
+const middlewares = jsonServer.defaults();
+const PORT = process.env.PORT || 3000;
+server.use(middlewares);
+server.use(router);
+server.listen(PORT, () => {
+  console.log("JSON Server is running");
+});
